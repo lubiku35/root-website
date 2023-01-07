d7e0e3b08a2944db22b4407cc0baaf9722090f76
Lubos Motosicky <lubiku@proton.me>
2023-01-07 21:01:32 +0100

    moving from html to php

---
+++ blog.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="Official website of Lubiku"/>
+
+    <!-- Cascading Style Sheets -->    
+    <link rel="stylesheet" href="./main.css">
+    <title>LUBIKU</title>
+</head>
+<body>
+    <!--===== MAIN HEADING SECTION =====-->
+    <?php
+        include_once "./sections/header/header.php"
+    ?>
+
+    <!--===== LANDING PAGE SECTION =====-->
+    <?php
+        include_once './sections/blog-landing/blog-landing.php'
+    ?>
+
+
+    <!--===== FOOTER SECTION =====-->
+    <?php
+        include_once './sections/footer/footer.php'
+    ?>
+
+    <script src="./scripts/theme-changer.js"></script>
+</body>
+</html>

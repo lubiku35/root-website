e3d14d697c5880f1ca789360f771d56407234073
Lubos Motosicky <lubiku@proton.me>
2023-01-13 17:41:16 +0100

    starting with login form

--- login.php
+++ login.php
@@ -20,8 +20,36 @@ session_start();
     include_once("./header.php")
         ?>
 
+    <?php
 
-    <?php echo $_SESSION["colormode"] ?>
+    ?>
+
+    <main id="login-container">
+        <h2>login</h2>
+        <form action="" method="post" id="login-form">
+            <div class="login-form-input-wrapper">
+                <div class="form-input-row">
+                    <label for="username">username / email:</label>
+                    <p class="required-form-input">*</p>
+                </div>
+                <input type="text" name="username">
+                <p id="login-username-server-error-message">CLIENT MESSAGE!</p>
+                <p id="login-username-client-error-message">SERVER MESSAGE!</p>
+            </div>
+            <div class="login-form-input-wrapper">
+                <div class="form-input-row">
+                    <label for="password">password</label>
+                    <p class="required-form-input">*</p>
+                </div>
+                <input type="text" name="password">
+                <p id="login-password-server-error-message">CLIENT MESSAGE!</p>
+                <p id="login-password-client-error-message">SERVER MESSAGE!</p>
+            </div>
+            <button type="submit">submit</button>
+        </form>
+        <h6 id="dont-have-acc">don't have an account ? <a href="./register.php">register here</a></h6>
+    </main>
+
 </body>
 
 </html>

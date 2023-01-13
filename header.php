25b34df451fb498ae097f3fb09f64edfbb81d68c
Lubos Motosicky <lubiku@proton.me>
2023-01-13 12:05:25 +0100

    first commit new version

---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+        <a href="./index.php">luboslav motosicky</a>
+        <ul>
+            <li><a href="./index.php#whoami">whoami</a></li>
+            <li><a href="./index.php#services">services</a></li>
+            <li><a href="./blog.php">blog</a></li>
+            <li><a href="./shop.php">shop</a></li>
+            <li><a href="./contact.php">contact</a></li>
+            <li><a href="./login.php">login</a></li>
+        </ul>
+        <img src="" alt="">
+    </nav>
+</header>
